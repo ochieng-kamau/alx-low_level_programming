110cd6c0ed48769ad898277eca7b6290a36c9dcf
Joseph Ochieng Kamau <ochiengkamauj@gmail.com>
2022-06-02 11:19:54 -0700

    Add 4-puts.c executable

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * * main will print out a string to stdout
+ * * 0 will be returned if successful
+ */
+int main(void)
+	{
+		puts("\"Programming is like building a multilingual puzzle");
+		return (0);
+
+	}
