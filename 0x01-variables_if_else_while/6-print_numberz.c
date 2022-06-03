4a3f41338ec82b379e5e05eb3d7ffa531e24d641
Joseph Ochieng Kamau <ochiengkamauj@gmail.com>
2022-06-03 13:58:15 -0700

    Add 6-print_numberz.c executable

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints base 10 single digit numbers
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
