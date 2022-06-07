1ec8f0056c940d4038250b057d2ba454912b1784
Joseph Ochieng Kamau <ochiengkamauj@gmail.com>
2022-06-07 11:42:01 -0700

    Modified 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * main - Prints the alphabet in lowercase then a new line
+ * print_alphabet - Prints the alphabet in lowercase then a new line
  *
  * Return: Always 0 (Success)
  */
