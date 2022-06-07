83c6d93f82946b553c58b665c9a9000457deb69f
Joseph Ochieng Kamau <ochiengkamauj@gmail.com>
2022-06-07 15:03:06 -0700

    Added 10-add.c executable

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @n1: first integer input
+ * @n2: second integer input
+ * Return: Always 0 (Success)
+ */
+int add(int n1, int n2)
+{
+	int result;
+
+	result = n1 + n2;
+	return (result);
+}
