fb4135c00cdd226effeed3e229ff54766dd196c1
Joseph Ochieng Kamau <ochiengkamauj@gmail.com>
2022-06-09 09:58:10 -0700

    Added 0-isupper.c, main.h, Readme files

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if character is in uppercase
+ * @c: the integer to be evaluated
+ * Return: Always 0 (Success)
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This segment expands on functions and nested loops
