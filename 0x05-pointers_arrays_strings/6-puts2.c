bbccd9acc271dd778bd04a7f5e31f2b96fad461f
Joseph Ochieng Kamau <ochiengkamauj@gmail.com>
2022-06-13 09:12:11 -0700

    Added 6-puts2.c file

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every character of a string
+ * @str: pointer to be updated
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int x;
+
+	for (x = 0; str[x] != '\0'; x++)
+	{
+		if (x % 2 == 0)
+		{
+			_putchar (str[x]);
+		}
+
+		_putchar ('\n');
+	}
+}
