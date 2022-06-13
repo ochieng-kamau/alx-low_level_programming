57067053b13df0d358e96236c90cbe98afd8c801
Joseph Ochieng Kamau <ochiengkamauj@gmail.com>
2022-06-13 08:46:14 -0700

    Added 4-print_rev.c file

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - reverses a string
+ * @s: pointer to be updated
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int a = 0;
+
+	while (s[a] != '\0')
+	{
+		a++;
+	}
+
+	for (a -= 1; a >= 0; a--)
+	{
+		_putchar(s[a]);
+	}
+
+	_putchar('\n');
+}
