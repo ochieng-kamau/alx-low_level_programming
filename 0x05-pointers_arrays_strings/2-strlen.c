34c32168c32ae0f42bdd31d3478c5c08c6aa5482
Joseph Ochieng Kamau <ochiengkamauj@gmail.com>
2022-06-13 06:59:17 -0700

    Added 2-strlen.c file

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: pointer to be updated
+ * Return: 0 (Success)
+ */
+
+int _strlen(char *s)
+{
+	int a;
+
+	for (a = 0; s[i] != '\0'; ++a)
+	{
+		continue;
+	}
+	printf("Length of the string: %d", a);
+
+	return (0);
+}
