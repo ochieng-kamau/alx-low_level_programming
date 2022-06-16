b9b556010ec382c127d40418acf36261a6cab901
Joseph Ochieng Kamau <ochiengkamauj@gmail.com>
2022-06-16 09:01:11 -0700

    Added 6-cap_string.c file

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * cap_string - converts all words of a string to uppercase
+ * @s: input string
+ * Return: uppercase string
+ */
+
+char *cap_string(char *s)
+{
+	int i, x;
+	int cap = 32;
+	int separators[] = {',', ';', '.', '?', '"',
+		'(', ')', '{', '}', ' ', '\n', '\t'};
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] - cap;
+		}
+
+		cap = 0;
+
+		for (x = 0; x <= 12; x++)
+		{
+			if (s[i] == separators[x])
+			{
+				x = 12;
+				cap = 32;
+			}
+		}
+	}
+
+	return (s);
+}
