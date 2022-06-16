46ea191b8d9121e219916ed0a5048ea89b5c7337
Joseph Ochieng Kamau <ochiengkamauj@gmail.com>
2022-06-16 07:03:35 -0700

    Added 3-strcmp.c file

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: string one
+ * @s2: string two
+ * Return: 0 (Success)
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int a;
+
+	for (a = 0; s1[a] != '\0' && s2[a] != '\0'; a++)
+	{
+		if (s1[a] != s2[a])
+			return (s1[a] - s2[a]);
+	}
+
+	return (0);
+}
