36c5499e03b61b055d0be0ef1ee604a602dd2e6d
Joseph Ochieng Kamau <ochiengkamauj@gmail.com>
2022-06-17 01:18:03 -0700

    Added 100-rot13.c file

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string using rot13
+ * @s: input string
+ * Return: encoded string
+ */
+
+char *rot13(char *s)
+{
+	int count = 0, x;
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	while (*(s + count) != '\0')
+	{
+		for (x = 0; x < 52; x++)
+		{
+			if (*(s + count) == alphabet[x])
+			{
+				*(s + count) = rot13[x];
+				break;
+			}
+		}
+		count++;
+	}
+
+	return (s);
+}
