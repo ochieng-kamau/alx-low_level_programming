091010698949c3b971c2aa5169502726c8389a59
Joseph Ochieng Kamau <ochiengkamauj@gmail.com>
2022-06-26 03:53:10 -0700

    Added 2-args.c file

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints all supplied arguments
+ * @argc: argument count
+ * @argv: arguments
+ * Return: 0
+ */
+
+int main(int argc, char argv[])
+{
+	int x;
+
+	for (x = 0; x < argc; x++)
+	{
+		printf("%s\n", argv[x]);
+	}
+
+	return (0);
+}
