a2ce61d786c6d1cdf6efff43c537659040c15926
Joseph Ochieng Kamau <ochiengkamauj@gmail.com>
2022-06-26 04:03:50 -0700

    Fixed bug in 2-args.c file

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -8,14 +8,12 @@
  * Return: 0
  */
 
-int main(int argc, char argv[])
+int main(int argc, char *argv[])
 {
 	int x;
 
 	for (x = 0; x < argc; x++)
-	{
 		printf("%s\n", argv[x]);
-	}
 
 	return (0);
 }
