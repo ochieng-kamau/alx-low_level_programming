2fb39056762618011820e81cb15fed07684850fd
Joseph Ochieng Kamau <ochiengkamauj@gmail.com>
2022-06-30 05:29:01 -0700

    Added 2-calloc.c file

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _calloc - allocates mem for nmeb array of size bytes
+ * @nmemb: number of elements
+ * @size: size of elements
+ * Return: pointer to alloc mem
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *c;
+	int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	c = malloc(nmemb * size);
+	if (c == NULL)
+		return (NULL);
+
+	for (i = 0; i < nmemb * size; i++)
+		c[i] = 0;
+
+	return (c);
+}
