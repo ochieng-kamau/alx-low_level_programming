da0b73e621fbdbf7216e7f2f50e5896a519538e7
Ochieng Kamau <ochiengkamauj@gmail.com>
2022-09-02 22:31:13 +0000

    Added 101-quote.c file

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdi0.h>
+#include <unistd.h>
+
+/**
+ * main - Program that prints a string 
+ * 	  using write
+ * Return: 1 to stderr
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
